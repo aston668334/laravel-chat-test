46b5832ad5ba5d2bbc7aecc9c609347ae0516cd2
aston668334 <aston668334.1@gmail.com>
2022-09-22 10:01:36 +0800

    php artisan migrate --seed

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -8,7 +8,7 @@ echo "🎬 artisan commands"
 
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
 php artisan migrate --no-interaction --force
-artisan migrate --seed
+php artisan migrate --seed
 echo "🎬 start supervisord"
 
 supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
