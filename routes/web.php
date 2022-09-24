d418a239861fb2307aebf18439b9bf7e559ce453
aston668334 <aston668334.1@gmail.com>
2022-09-24 14:02:26 +0800

    force https

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-if (App::environment('production')) {
-    URL::forceScheme('https');
-}
 
 Route::get('/', function () {
     return view('welcome');
